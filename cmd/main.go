86a09f6b0f5377ecdacb33ff83c4af6098618cd4
Allan Silva <allan.silva@db1.com.br>
2023-05-01 17:38:48 -0300

    Agrupa rotas de Lancamento no pacote de inbound

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"log"
 
+	"github.com/allanfs/balance-server/internal/app/inbound"
 	"github.com/allanfs/balance-server/internal/app/infra"
 	"github.com/gofiber/fiber/v2"
 )
@@ -23,23 +24,12 @@ func main() {
 	api := app.Group("api")
 	api_v1 := api.Group("v1")
 
-	lancamentosGroup := api_v1.Group("lancamentos")
-	lancamentosGroup.Post("/", func(c *fiber.Ctx) error {
-		return c.SendString("Rota cadastrar lançamentos")
-	})
-	lancamentosGroup.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Rota obters lançamentos")
-	})
-
-	lancamentosGroup.Get("/:id", func(c *fiber.Ctx) error {
-		return c.SendString("Rota de obter lançamento por id: " + c.Params("id"))
-	})
+	inbound.SetLancamentosRoutes(api_v1.Group("lancamentos"))
 
 	app.Listen(":3000")
 }
 
 func FinishHim(err error) {
-
 	if err != nil {
 
 		ie := new(infra.InfraError)

7cb4111c196f73374bf17a5b138c003b89645c3e
Allan Silva <allan.silva@db1.com.br>
2023-05-04 22:45:15 -0300

    comenta colunas da tabela balance.entry

--- internal/migration/1_init.sql
+++ internal/migration/1_init.sql
@@ -10,3 +10,10 @@ create table balance.entry (
     external_info varchar(255),
     created_at timestamp not null default now()
 );
+
+COMMENT ON COLUMN balance.entry.external_info IS 'user''s correlation info to track this entry';
+COMMENT ON COLUMN balance.entry.entry_type IS 'enum: C to Credit; D to Debit. This enum is maintained by application';
+COMMENT ON COLUMN balance.entry.amount IS 'entry amount value';
+COMMENT ON COLUMN balance.entry."name" IS 'entry name';
+
+CREATE INDEX entry_external_info_idx ON balance.entry (external_info);
